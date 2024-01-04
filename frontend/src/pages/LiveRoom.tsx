3784774baecc11103e44021c2e6f3f995b33aad7
yihsuan <yiiihsuann@gmail.com>
2024-01-04 18:50:20 +0800

    feat: live room, productId set, title, shopping navigate and coupon get

--- frontend/src/pages/LiveRoom.tsx
+++ frontend/src/pages/LiveRoom.tsx
@@ -7,8 +7,10 @@ import { Room, Participant, TrackPublication, RoomEvent } from "livekit-client";
 import styled from 'styled-components';
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
-import jwt from 'jsonwebtoken';
-
+import { frontendurl } from "../constants/urls";
+import { socketurl } from "../constants/urls";
+import io from 'socket.io-client';
+import { FaTicketAlt, FaShoppingCart } from 'react-icons/fa';
 
 
 import { FaExternalLinkAlt } from 'react-icons/fa';
@@ -21,8 +23,6 @@ import {
   VideoConference,
   useLiveKitRoom,
   useTracks,
-  FocusLayout,
-  FocusLayoutContainer,
 } from '@livekit/components-react';
 
 import { Track } from "livekit-client";
@@ -54,28 +54,59 @@ const StyledControlBar = styled(ControlBar)`
 
 const StyledJoinMessages = styled.div`
   position: absolute;
-  top: 10px;
-  left: 10px;
-  z-index: 100; 
+  top: 100px;
+  left: 100px;
+  z-index: 5000; 
   color: white;
-  background-color: rgba(0, 0, 0, 0.5); // 半透明背景
-  padding-left: 5px;
+  //background-color: rgba(0, 0, 0, 0.5); // 半透明背景
+  padding-left: 10px;
 `;
 
 const VideoContainerWithMargin = styled(VideoContainer)`
   margin-top: 111px; 
   height: 85vh; 
   overflow: hidden;
+  
+`;
+
+const LiveHostInfo = styled.div`
+  position: absolute;
+  bottom: 38px;
+  left: 20px;
+  z-index: 1000;
+  background-color: rgba(0, 0, 0, 0.5); 
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 14px;
+  line-height: 1.5;
 `;
 
 
-function MyVideoConference() {
-  // `useTracks` returns all camera and screen share tracks. If a user
-  // joins without a published camera track, a placeholder track is returned.
+const TitleBar = styled.div`
+  position: absolute;
+  top: 0;
+  margin-top:2%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 10;
+  background-color: rgba(50, 50, 50, 0.7);
+  color: #FFFFFF; 
+  //color: #333;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-size: 18px;
+  line-height: 1.5;
+  box-shadow: 0 2px 5px rgba(0,0,0,0.2);
+  text-align: center;
+  width: 98.5%; 
+`;
 
-  const [productID, setProductID] = useState(""); // 用于存储商品ID
-  const [showCartButton, setShowCartButton] = useState(false);
-  const [showInput, setShowInput] = useState(false);
+
+function MyVideoConference({ showInput, productID, setProductID, handleSubmitProductID, cartLink, showCartButton }) {
+  const [showCoupon, setShowCoupon] = useState(true);
+
+  const { user } = useUser();
 
 
   const tracks = useTracks(
@@ -88,85 +119,102 @@ function MyVideoConference() {
   );
 
   const initiatorIdentity = 'user-yh';
+  const initiatorTrack = tracks.find(track => track?.participant?.identity === initiatorIdentity);
 
-  //處理商品id的邏輯
-  const handleSubmit = () => {
-    // 輸入的id存在productId
-    if (productID) {
-      setShowCartButton(true);
+
+
+  // If the initiator's track is not found, show a loading message or a placeholder
+  if (!initiatorTrack) {
+    return <div>直播尚未開始 請稍候...</div>;
+  }
+
+
+  const handleCouponClick = async () => {
+    if (!user) {
+      console.error('No user found');
+      return;
+    }
+
+    const userId = user.id;
+    console.log('user get reward is ', userId)
+    const rewardId = 7;
+
+    try {
+      const response = await fetch(`${backendurl}/user/reward`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user_id: userId, reward_id: rewardId })
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Successfully received response:', data);
+      setShowCoupon(false);
+    } catch (error) {
+      console.error('Error during fetching:', error);
     }
   };
-
-  // const focusTrack = tracks.find(track => track.participant.identity === initiatorIdentity);
-  // const otherTracks = tracks.filter(track => track.participant.identity !== initiatorIdentity);
-  const initiatorTrack = tracks.find(track => track.participant.identity === initiatorIdentity);
-  const otherTracks = tracks.filter(track => track.participant.identity !== initiatorIdentity);
-
-  // useEffect(() => {
-  //   const storage = window.localStorage;
-  //   const token = storage.getItem("token");
-
-  //   const decodeToken = (token) => {
-  //     try {
-  //       const decoded = jwt.decode(token);
-  //       return decoded;
-  //     } catch (error) {
-  //       console.error('解析error:', error);
-  //       return null;
-  //     }
-  //   };
-
-  //   if (token) {
-  //     const decodedToken = decodeToken(token);
-  //     if (decodedToken && decodedToken.id === 10243) {
-  //       setShowInput(true);
-  //     }
-  //   }
-  // }, []);
-
-
 
 
   return (
 
     <div style={{ position: 'relative', height: '100%' }}>
 
+      <TitleBar>
+        Stylish試衣間 | 限時特惠中
+      </TitleBar>
 
-      <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height) ) ' }}>
-        <div>
-          {tracks.map(track => {
-            // 只為發起者渲染頭像
-            if (track.participant.identity === initiatorIdentity) {
-              return <ParticipantTile key={track.sid} track={track} />;
-            }
-            return null;  // 不渲染其他參與者的頭像
-          })}
+      {showInput && (
+        <div style={{ position: 'absolute', bottom: '100px', left: '180px', zIndex: 6000, backgroundColor: 'white', padding: '1px', borderRadius: '5px', opacity: 0.7 }}>
+          <input
+            type="text"
+            value={productID}
+            onChange={(e) => setProductID(e.target.value)}
+            placeholder="Enter Product ID"
+            style={{ padding: '5px', margin: '5px' }}
+          />
+          <button onClick={handleSubmitProductID} style={{ padding: '5px', margin: '5px' }}>Create Cart Link</button>
+          {cartLink && <a href={cartLink} target="_blank" rel="noopener noreferrer" style={{ display: 'block', marginTop: '10px' }}>Go to Cart</a>}
         </div>
+      )}
+
+
+      <GridLayout tracks={[initiatorTrack]} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+        {initiatorTrack && <ParticipantTile key={initiatorTrack.sid} track={initiatorTrack} />}
       </GridLayout>
 
+      <LiveHostInfo>
+        <div>主播主: yh</div>
+        <div>身高: 155 cm</div>
+        <div>體重: 50 kg</div>
+      </LiveHostInfo>
+
+      {showCoupon && (
+        <img
+          src="coupon6.png"
+          alt="優惠卷"
+          style={{ position: 'absolute', top: 100, right: 30, width: '140px', height: '90px', zIndex: 10, opacity: 0.8 }}
+          onClick={handleCouponClick}
+        />
+      )}
+
+      {showCartButton && (
+        <a href={cartLink} target="_blank" rel="noopener noreferrer"
+          style={{ position: 'absolute', top: 200, right: 30, zIndex: 10, opacity: 0.9 }}>
+          <img src="order3.png" alt="購物連結" style={{ width: '130px', height: '130px' }} />
+        </a>
+      )}
 
 
-      <a href="https://example.com" target="_blank" rel="noopener noreferrer"
-        style={{ position: 'absolute', top: 400, right: 100, zIndex: 1000 }}>
-        <img src="coupon.png" alt="優惠卷" style={{ width: '100px', height: '100px' }} />
-      </a>
-
-      <a href="https://example.com" target="_blank" rel="noopener noreferrer"
-        style={{ position: 'absolute', top: 500, right: 100, zIndex: 1000 }}>
-        <img src="cart.png" alt="購物連結" style={{ width: '80px', height: '80px' }} />
-      </a>
 
 
     </div>
 
-
-
-
-    // <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
-    //   {/* The GridLayout accepts zero or one child. The child is used
-    //   as a template to render all passed in tracks. */}
-    //   {/* <ParticipantTile /> */}
-    // </GridLayout>
   );
 }
 
@@ -180,43 +228,89 @@ function MyVideoConference() {
 
 export default function LiveRoom() {
 
+  const { user } = useUser();
+
+
   const [token, setToken] = useState(null);
   const [joinMessages, setJoinMessages] = useState([]);
   const [displayMessage, setDisplayMessage] = useState("");
-
-  // useEffect(() => {
-  //   const room = new Room();
-
-  //   const handleParticipantConnected = participant => {
-  //     const message = `${participant.identity} 加入了房間`;
-  //     setDisplayMessage(message);
-  //     setTimeout(() => setDisplayMessage(""), 2000); 
-  //   };
-
-  //   const handleParticipantDisconnected = participant => {
-  //     const message = `${participant.identity} 離開了房間`;
-  //     setDisplayMessage(message);
-  //     setTimeout(() => setDisplayMessage(""), 2000); 
-  //   };
+  const [showInput, setShowInput] = useState(false);
+  const [productID, setProductID] = useState("");
+  const [cartLink, setCartLink] = useState("");
+  const [showCartButton, setShowCartButton] = useState(false);
+  const [socket, setSocket] = useState(null);
 
 
 
-  //   room.on(RoomEvent.ParticipantConnected, handleParticipantConnected);
-  //   room.on(RoomEvent.ParticipantDisconnected, handleParticipantDisconnected);
-
-  //   if (token) {
-  //     room.connect(serverUrl, token).catch(console.error);
-  //   }
-
-  //   return () => {
-  //     room.disconnect();
-  //   };
-  // }, [serverUrl, token]);
+  useEffect(() => {
+    console.log('User ID:', user?.id);
+    if (user?.id === 10243) {
+      setShowInput(true);
+    } else {
+      setShowInput(false);
+    }
+  }, [user]);
 
 
 
+  useEffect(() => {
+    console.log('Setting up socket connection...');
+    const newSocket = io(socketurl);
 
-  // 前端使用 fetch 請求 token
+    newSocket.on('connect', () => {
+      console.log('Socket connected');
+    });
+
+    newSocket.on('updateProduct', (productId) => {
+      console.log('Received updateProduct event with productId:', productId);
+      const cartLink = `${frontendurl}/products/${productId}`;
+      //const cartLink = 'http:localhost:5173/products/${productId}';
+
+      setProductID(productId);
+      setShowCartButton(true);
+      setCartLink(cartLink);
+    });
+
+    newSocket.on('userJoined', (userName) => {
+      console.log(`User joined: ${userName}`);
+      setDisplayMessage(`${userName} joined the room`);
+      setTimeout(() => setDisplayMessage(''), 2000);
+    });
+
+    newSocket.on('userLeft', (userName) => {
+      console.log(`User left: ${userName}`);
+      setDisplayMessage(`${userName} left the room`);
+      setTimeout(() => setDisplayMessage(''), 2000);
+    });
+
+
+    newSocket.on('disconnect', () => {
+      console.log('Socket disconnected');
+    });
+
+    setSocket(newSocket); // Store the socket in the state variable
+
+    return () => {
+      console.log('Cleaning up socket...');
+      newSocket.disconnect();
+    };
+  }, []);
+
+
+
+
+  const handleSubmitProductID = () => {
+    if (user?.id === 10243 && productID && socket) {
+      console.log('Emitting broadcastProductId event with productId:', productID);
+      socket.emit('broadcastProductId', productID);
+      setShowCartButton(true);
+    } else {
+      console.log('Socket not available or productID not set');
+    }
+  };
+
+
+
   useEffect(() => {
     const storage = window.localStorage;
     const token = storage.getItem("token"); // 這是用戶的登入 token，不是 livekit 的 token
@@ -229,7 +323,7 @@ export default function LiveRoom() {
     })
       .then(response => response.json())
       .then(data => {
-        setToken(data.token); // 假設後端響應中包含了生成的 livekit token
+        setToken(data.token);
       })
       .catch(error => {
         console.error('獲取令牌時出錯:', error);
@@ -237,10 +331,11 @@ export default function LiveRoom() {
   }, []);
 
 
-
   if (!token) {
-    return <div>Loading...</div>;
+    return <div>Loading...請登入！！</div>;
   }
+
+
 
   return (
     <div>
@@ -253,10 +348,16 @@ export default function LiveRoom() {
           serverUrl={serverUrl}
           data-lk-theme="default"
         >
-
           <div style={{ display: 'flex', height: '100%' }}>
             <div style={{ flex: 3 }}>
-              <MyVideoConference />
+              <MyVideoConference
+                showInput={showInput}
+                productID={productID}
+                setProductID={setProductID}
+                handleSubmitProductID={handleSubmitProductID}
+                cartLink={cartLink}
+                showCartButton={showCartButton}
+              />
               <RoomAudioRenderer />
               <StyledControlBar />
             </div>
@@ -265,9 +366,6 @@ export default function LiveRoom() {
             </StyledChatContainer>
           </div>
         </LiveKitRoom>
-        {/* <StyledJoinMessages>
-        {displayMessage && <div>{displayMessage}</div>}
-      </StyledJoinMessages> */}
       </VideoContainerWithMargin>
       <Footer />
     </div>
